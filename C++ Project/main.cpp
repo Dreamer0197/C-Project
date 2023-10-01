e31095367db5625fbb116bc90b4f8b8cc95ce0a9
Kaan Şengün <427642@ogr.ktu.edu.tr>
2023-10-01 13:50:33 +0300

    Initial Commit

---
+++ C++ Project/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  C++ Project
+//
+//  Created by Kaan Şengün on 1.10.2023.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
